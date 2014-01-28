f183e0ff4fcb031d9beec2b7b3a15c0c4ebb0924
Sergio Benitez <sbenitez@mit.edu>
2014-01-28 11:28:26 -0500

    Added a random byte array generation function.

--- src/test/main.go
+++ src/test/main.go
@@ -1,8 +1,24 @@
 package main
 
-// import "fmt"
-import "gofs"
-import "fmt"
+import (
+  "gofs"
+  "fmt"
+  "math/rand"
+)
+
+func randBytes(n int) []byte {
+  if (n & 0x3) != 0 { panic("n must be a multiple of 4") }
+
+  out := make([]byte, n)
+  for i := 0; i < n / 4; i += 1 {
+    rand32 := rand.Uint32()
+    for j := 0; j < 4; j += 1 {
+      out[i * 4 + j] = byte((rand32 >> (uint(j) * 8)) & 0xFF)
+    }
+  }
+
+  return out
+}
 
 func main() {
   proc := gofs.InitProc()
@@ -11,9 +27,13 @@ func main() {
   fd, err := proc.Open("file", gofs.O_RDWR | gofs.O_CREAT, gofs.UserMode())
   if err != nil { fmt.Println(err) }
 
+  rand1 := randBytes(2500)
+  rand2 := randBytes(4800)
+  fmt.Println(rand1)
+
   file := proc.GetFile(fd)
-  file.Write([]byte("Hello, world!"))
-  file.Write([]byte("Hi, hi!"))
+  file.Write(rand1)
+  file.Write(rand2)
 
   buffer := make([]byte, 11)
   file.Seek(0, gofs.SEEK_SET)
