5b061b2f8d573cd90405c4f7734478c41c5782b0
Sergio Benitez <sbenitez@mit.edu>
2014-05-08 21:45:31 -0400

    Updated benchmarking script to use CFS and new Go runner script.

--- bench.sh
+++ bench.sh
@@ -25,23 +25,37 @@ elif [ "$flag" == '-c' ]; then
 else
   gores=`mktemp -t goresXXXXX`
   echo "Running Go benchmarks...(output at $gores)"
-  go test -bench . bench > $gores 2> /dev/null
+
+  # Old way to run without runner
+  # go test -bench . bench > $gores 2> /dev/null
+
+  go install runner
+  ./bin/runner > $gores
+
   echo -e "Done. See $gores for output.\n"
 
   cres=`mktemp -t cresXXXXX`
   echo "Running C benchmarks...(output at $cres)"
-  gcc -std=gnu99 src/cbench/*.c && ./a.out > $cres
-  rm a.out
+
+  # Code to run Linux tmpfs benchmarks
+  # gcc -std=gnu99 src/cbench/*.c && ./a.out > $cres
+  # rm a.out
+
+  # Code to run against CFS benchmarks
+  pushd src/cfs/bench > /dev/null
+  ./run.sh > $cres
+  popd > /dev/null
+
   echo -e "Done. See $cres for output.\n"
 
   cleanc=`mktemp -t cleancXXXXX`
   cat $cres | grep Real | awk '{ print $3 }' | cut -dn -f1 > $cleanc
 
   cleango=`mktemp -t cleangoXXXXX`
-  cat $gores | head -n -1 | tail -n +2 | awk '{ print $3 }' > $cleango
+  cat $gores | awk '{ print $3 }' > $cleango
 
   headers=`mktemp -t headersXXXXX`
-  cat $gores | head -n -1 | tail -n +2 | awk '{ print $1 }' > $headers
+  cat $gores | awk '{ print $1 }' > $headers
 
   echo -e "Temp files: $cleanc, $cleango, $headers\n"
   echo "Results: (Go | C | ratio)"
